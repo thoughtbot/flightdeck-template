cf4a8aa978c3b9d42dc670c474442e7f090b987e
Joe Ferris <jferris@thoughtbot.com>
2022-02-24 12:14:58 -0500

    Add STATE_PREFIX template variable

--- network/production/backend.tf
+++ network/production/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-__PRODUCTION_ACCOUNT_ID__"
     dynamodb_table = "terraform-state-__PRODUCTION_ACCOUNT_ID__"
     encrypt        = true
-    key            = "network/production/terraform.tfstate"
+    key            = "__STATE_PREFIX__network/production/terraform.tfstate"
     kms_key_id     = "arn:aws:kms:__REGION__:__PRODUCTION_ACCOUNT_ID__:alias/terraform-state-__PRODUCTION_ACCOUNT_ID__"
     region         = "__REGION__"
     role_arn       = "arn:aws:iam::__PRODUCTION_ACCOUNT_ID__:role/terraform-state-__PRODUCTION_ACCOUNT_ID__"
--- platform/production-v1/backend.tf
+++ platform/production-v1/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-__PRODUCTION_ACCOUNT_ID__"
     dynamodb_table = "terraform-state-__PRODUCTION_ACCOUNT_ID__"
     encrypt        = true
-    key            = "platform/production-v1/terraform.tfstate"
+    key            = "__STATE_PREFIX__platform/production-v1/terraform.tfstate"
     kms_key_id     = "arn:aws:kms:__REGION__:__PRODUCTION_ACCOUNT_ID__:alias/terraform-state-__PRODUCTION_ACCOUNT_ID__"
     region         = "__REGION__"
     role_arn       = "arn:aws:iam::__PRODUCTION_ACCOUNT_ID__:role/terraform-state-__PRODUCTION_ACCOUNT_ID__"
