7d24355561cbf86d06c97179a5b900f52ce1a7e1
Joe Ferris <jferris@thoughtbot.com>
2022-02-26 10:47:01 -0500

    Add cluster names to ingress templates

--- ingress/production/main.tf
+++ ingress/production/main.tf
@@ -2,6 +2,7 @@ module "ingress" {
   providers = { aws.cluster = aws, aws.route53 = aws }
   source    = "github.com/thoughtbot/flightdeck//aws/ingress?ref=v0.5.0"
 
+  cluster_names       = ["__ORG_NAME__-production-v1"]
   hosted_zone_name    = "__PRODUCTION_HOSTED_ZONE__"
   name                = "__ORG_NAME__-production"
   network_tags        = { "kubernetes.io/cluster/__ORG_NAME__-production-v1" = "shared" }
--- ingress/sandbox/main.tf
+++ ingress/sandbox/main.tf
@@ -2,6 +2,7 @@ module "ingress" {
   providers = { aws.cluster = aws, aws.route53 = aws }
   source    = "github.com/thoughtbot/flightdeck//aws/ingress?ref=v0.5.0"
 
+  cluster_names       = ["__ORG_NAME__-sandbox-v1"]
   hosted_zone_name    = "__SANDBOX_HOSTED_ZONE__"
   name                = "__ORG_NAME__-sandbox"
   network_tags        = { "kubernetes.io/cluster/__ORG_NAME__-sandbox-v1" = "shared" }
