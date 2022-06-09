df19b9a0c4366e469fc1af471fa78709a9114007
Joe Ferris <jferris@thoughtbot.com>
2022-06-09 09:08:09 -0400

    Use different CIDR blocks for sandobx/production

--- network/production/main.tf
+++ network/production/main.tf
@@ -4,15 +4,15 @@ module "network" {
   cluster_names          = ["__ORG_NAME__-production-v1"]
   name                   = "__ORG_NAME__-production"
   nat_availability_zones = ["__REGION__a", "__REGION__b"]
-  vpc_cidr_block         = "10.100.0.0/16"
+  vpc_cidr_block         = "10.32.0.0/16"
 
   private_subnet_cidr_blocks = {
-    "__REGION__a" = "10.100.64.0/18"
-    "__REGION__b" = "10.100.128.0/18"
+    "__REGION__a" = "10.32.64.0/18"
+    "__REGION__b" = "10.32.128.0/18"
   }
 
   public_subnet_cidr_blocks = {
-    "__REGION__a" = "10.100.0.0/20"
-    "__REGION__b" = "10.100.16.0/20"
+    "__REGION__a" = "10.32.0.0/20"
+    "__REGION__b" = "10.32.16.0/20"
   }
 }
--- network/sandbox/main.tf
+++ network/sandbox/main.tf
@@ -4,15 +4,15 @@ module "network" {
   cluster_names          = ["__ORG_NAME__-sandbox-v1"]
   name                   = "__ORG_NAME__-sandbox"
   nat_availability_zones = ["__REGION__a"]
-  vpc_cidr_block         = "10.100.0.0/16"
+  vpc_cidr_block         = "10.16.0.0/16"
 
   private_subnet_cidr_blocks = {
-    "__REGION__a" = "10.100.64.0/18"
-    "__REGION__b" = "10.100.128.0/18"
+    "__REGION__a" = "10.16.64.0/18"
+    "__REGION__b" = "10.16.128.0/18"
   }
 
   public_subnet_cidr_blocks = {
-    "__REGION__a" = "10.100.0.0/20"
-    "__REGION__b" = "10.100.16.0/20"
+    "__REGION__a" = "10.16.0.0/20"
+    "__REGION__b" = "10.16.16.0/20"
   }
 }
