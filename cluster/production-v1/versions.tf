527cb35ae083176fd97861561ecb2ca387c9aa7e
Joe Ferris <jferris@thoughtbot.com>
2023-04-24 16:38:06 -0400

    Add required_version for Terraform modules

--- cluster/production-v1/versions.tf
+++ cluster/production-v1/versions.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.4.0"
+
   required_providers {
     aws = { version = "~> 4.0" }
     tls = { version = "~> 3.0" }
--- platform/production-v1/versions.tf
+++ platform/production-v1/versions.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.4.0"
+
   required_providers {
     aws        = { version = "~> 4.0" }
     helm       = { version = "~> 2.1" }
